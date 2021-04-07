92bd1450e236f8d9934f69d8fa7683f547fddf04
Kim Ilyong <kimxilxyong@gmail.com>
2021-04-07 10:15:52 +0200

    Added WORD and Void * tests

--- testarch.c
+++ testarch.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <inttypes.h>
 #include <limits.h>
 
 int main(void) {
@@ -11,5 +12,27 @@ int main(void) {
 #else
   printf("Unknown pointer size or missing size macros!\n");
 #endif
+  
+#if __WORDSIZE == 64
+  printf("Word is 64bit\n");
+#elif __WORDSIZE == 32
+  printf("Word is 32bit\n");
+#else
+  printf("Unknown word size or missing size macros!\n");
+#endif
 
+  if(sizeof (void *) * CHARBIT == 64) {
+    printf("Void * is 64bit\n");
+  } else {
+    printf("Void * is NOT 64bit\n");
+  }
+  
+#if UINTPTR_MAX == 0xffffffff
+  printf("UINTPTR_MAX is 32bit\n");
+#elif UINTPTR_MAX == 0xffffffffffffffff
+  printf("UINTPTR_MAX is 64bit\n");
+#else
+  printf("UINTPTR_MAX is WTF!\n");
+#endif
+  
 }
